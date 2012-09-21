5273b9754432cf32542b5a57894dbc046be63ad6
skkmania <skkmania@e420>
2012-09-21 23:32:27 +0900

    changed message

---
+++ SampleProj/src/jp/tuyano/eclipsebook/Sample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package jp.tuyano.eclipsebook;
+
+/**
+ * @author skkmania
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is Cool.");
+	}
+
+}
